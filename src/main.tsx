9c3d12ceadcf51ff670991c6a08a73c302275e5f
DEVLevid <levidlima80@gmail.com>
2024-05-27 16:51:30 -0300

    fix: Adding BrowserRouter for aplication.

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 import { App } from './App.tsx'
 import { ThemeProvider } from 'styled-components'
 import { DefaultTheme } from './assets/styles/theme/default.ts'
@@ -7,9 +8,11 @@ import { GlobalStyles } from './assets/styles/global.ts'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ThemeProvider theme={DefaultTheme}>
-      <App />
-      <GlobalStyles />
-    </ThemeProvider>
+    <BrowserRouter>
+      <ThemeProvider theme={DefaultTheme}>
+        <App />
+        <GlobalStyles />
+      </ThemeProvider>
+    </BrowserRouter>
   </React.StrictMode>,
 )
