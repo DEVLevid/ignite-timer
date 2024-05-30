eaf7c057de1d94a93b262efa97995e9b19223ef3
DEVLevid <levidlima80@gmail.com>
2024-05-30 17:51:42 -0300

    feat: Creating Header component.

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,20 @@
+import { HeaderContainer } from './styles'
+import logo from '../../assets/Logo.svg'
+import { Scroll, Timer } from 'phosphor-react'
+import { NavLink } from 'react-router-dom'
+
+export function Header() {
+  return (
+    <HeaderContainer>
+      <img src={logo} alt="logo" />
+      <nav>
+        <NavLink to="/">
+          <Timer size={24} />
+        </NavLink>
+        <NavLink to="/history">
+          <Scroll size={24} />
+        </NavLink>
+      </nav>
+    </HeaderContainer>
+  )
+}
---
+++ src/components/Header/styles.ts
@@ -0,0 +1,35 @@
+import styled from 'styled-components'
+
+export const HeaderContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  nav {
+    display: flex;
+    align-items: center;
+    gap: 0.5rem;
+
+    a {
+      width: 3rem;
+      height: 3rem;
+
+      display: flex;
+      align-items: center;
+      justify-content: center;
+
+      color: ${(props) => props.theme['gray-100']};
+
+      border-bottom: 3px solid transparent;
+      border-top: 3px solid transparent;
+
+      &:hover {
+        border-bottom: 3px solid ${(props) => props.theme['green-500']};
+      }
+
+      &.active {
+        color: ${(props) => props.theme['green-500']};
+      }
+    }
+  }
+`
