a7156ec754f15f8ad66b07242fc74c0b2252a2de
DEVLevid <levidlima80@gmail.com>
2024-05-30 18:47:22 -0300

    fix: Fixing collors in global styles.

--- src/assets/styles/theme/default.ts
+++ src/assets/styles/theme/default.ts
@@ -4,7 +4,7 @@ export const DefaultTheme = {
   'gray-100': '#E1E1E6',
   'gray-300': '#C4C4C6',
   'gray-400': '#BD8D99',
-  'gray-500': '#7C7CBA',
+  'gray-500': '#7C7C8A',
   'gray-600': '#323238',
   'gray-700': '#29292E',
   'gray-800': '#1d1d1f',
