89397e1668092763a8291f4d161b2cc5407e6c11
DEVLevid <levidlima80@gmail.com>
2024-05-30 18:47:50 -0300

    feat: Add Home component.

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,42 @@
+import { Play } from 'phosphor-react'
+import {
+  CountDownContainer,
+  FormContainer,
+  HomeContainer,
+  MinutesAmount,
+  Separator,
+  StartCountDownButton,
+  TaskInput,
+} from './styles'
+
+export default function Home() {
+  return (
+    <HomeContainer>
+      <form action="">
+        <FormContainer>
+          <label htmlFor="task">Vou trabalhar em</label>
+          <TaskInput
+            type="text"
+            id="task"
+            placeholder="Dê um nome para o seu projeto"
+          />
+          <label htmlFor="minutesAmount">durante</label>
+          <MinutesAmount type="number" id="minutesAmount" placeholder="00" />
+          <span>minutos.</span>
+        </FormContainer>
+
+        <CountDownContainer>
+          <span>0</span>
+          <span>0</span>
+          <Separator>:</Separator>
+          <span>0</span>
+          <span>0</span>
+        </CountDownContainer>
+
+        <StartCountDownButton type="submit">
+          <Play size={24} /> Começar
+        </StartCountDownButton>
+      </form>
+    </HomeContainer>
+  )
+}
