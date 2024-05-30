1aff046a35a45966e0cc9ed5221dd5cd0122375f
DEVLevid <levidlima80@gmail.com>
2024-05-30 20:01:37 -0300

    feat: Create History component.

---
+++ src/pages/History/index.tsx
@@ -0,0 +1,72 @@
+import { HistoryContainer, HistoryList, Status } from './styles'
+
+export function History() {
+  return (
+    <HistoryContainer>
+      <h1>Meu Histórico</h1>
+
+      <HistoryList>
+        <table>
+          <thead>
+            <tr>
+              <th>Tarefa</th>
+              <th>Duração</th>
+              <th>Início</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>ha 2 meses</td>
+              <td>
+                <Status statusColor="green">Concluido</Status>
+              </td>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>ha 2 meses</td>
+              <td>
+                <Status statusColor="yellow">Em Andamento</Status>
+              </td>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>ha 2 meses</td>
+              <td>
+                <Status statusColor="red">interrompido</Status>
+              </td>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>ha 2 meses</td>
+              <td>
+                <Status statusColor="green">Concluido</Status>
+              </td>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>ha 2 meses</td>
+              <td>
+                <Status statusColor="yellow">Em andamento</Status>
+              </td>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>ha 2 meses</td>
+              <td>
+                <Status statusColor="green">Concluido</Status>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </HistoryList>
+    </HistoryContainer>
+  )
+}
